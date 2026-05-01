b6ecc3846939722c48d09d0ee502625e5ff43ae7
Chua Yi Cheng <yichengchua0717@gmail.com>
2026-05-01 16:45:05 +0800

    animation

--- main.py
+++ main.py
@@ -56,21 +56,27 @@ class PetWindow(QWidget):
         self.label.setAttribute(Qt.WA_TranslucentBackground)
         self.label.setStyleSheet("background: transparent;")
         
-        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        self.movie = QMovie(os.path.join(BASE_DIR, "happy.gif"))
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.current_gif = ""
+
+        self.movie = QMovie(os.path.join(self.BASE_DIR, "firefly-hsr-firefly.gif"))
+        print("GIF valid:", self.movie.isValid())
         self.movie.setScaledSize(QSize(200, 200))
+        self.movie.setCacheMode(QMovie.CacheAll)
+        self.movie.setSpeed(100)
+
         self.label.setMovie(self.movie)
         self.movie.start()
-        self.movie.frameChanged.connect(self.update_size)
-
+        self.update_size()
+        self.current_gif = os.path.join(self.BASE_DIR, "firefly-hsr-firefly.gif")
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setStyleSheet("background: transparent;")
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.game_loop)
-        self.timer.start(1000)
-
+        self.timer.start(200)
+  
         self.drag_pos = None
         self.todo_window = None
 
@@ -85,7 +91,7 @@ class PetWindow(QWidget):
         state = self.pet.state
 
         if state == "happy":
-            new_file = "happy.gif"
+            new_file = "firefly-hsr-firefly.gif"
         elif state == "hungry":
             new_file = "hungry.gif"
         elif state == "starving":
@@ -100,9 +106,13 @@ class PetWindow(QWidget):
             new_file = "sleep.gif"
         else:
             new_file = "happy.gif"
+        
+        new_path = os.path.join(self.BASE_DIR, new_file)
 
-        if self.movie.fileName() != new_file:
-            self.movie.setFileName(new_file)
+        if self.current_gif != new_path:
+            self.current_gif = new_path
+            self.movie.stop()
+            self.movie.setFileName(new_path)
             self.movie.start()
 
     # ================= DRAG + CLICK =================
